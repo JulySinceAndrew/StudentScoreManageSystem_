c719d4f5b48e4c4292e77a1a042dae90fe80b8ff
JulySinceAndrew <qit16@mails.tsinghua.edu.cn>
2017-08-20 21:39:33 +0800

    注释lesid

--- mainwindow_student.h
+++ mainwindow_student.h
@@ -40,7 +40,7 @@ private slots:
 private:
     Ui::MainWindow_Student *ui;
     long stuid;
-    long lesid; //没用？
+    //long lesid;
     Student* a_student;
     vector_of_Lesson lesson;
     vector_of_Teacher teacher;
